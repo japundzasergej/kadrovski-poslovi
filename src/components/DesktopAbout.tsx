85c8ac91225f4cb6eba1746913f1e1a20d74c91d
SERGEJ_JAPUNDZA <japundzasergej@gmail.com>
2023-12-09 14:45:49 +0100

    fix

--- src/components/DesktopAbout.tsx
+++ src/components/DesktopAbout.tsx
@@ -1,3 +1,5 @@
+import Paragraph from './Paragraph';
+
 const DesktopAbout = () => {
   const desktopAbout = (
     <section className="flex w-full">
@@ -5,23 +7,7 @@ const DesktopAbout = () => {
         <div className="bg-black h-1/3 items-center text-center text-white flex justify-start text-6xl w-[200%] py-48 font-tenor">
           <h1 className="ml-20">Услуге које пружамо</h1>
         </div>
-        <div className="bg-gray-100 h-2/3 flex justify-center items-center text-start text-2xl py-64 px-48">
-          <ul className="list-disc">
-            <li>
-              Саветовање и израда правилника о организацији и систематизацији
-              послова.
-            </li>
-            <li>Саветовања и израда правилника о раду и општих аката.</li>
-            <li>
-              Израда уговора о раду/анекса уговора о раду/уговора о обављања
-              привремених и повремених послова/уговора о делу.
-            </li>
-            <li>Израда свих решења из области радног односа.</li>
-            <li>Пријаве и одјаве запослених у CROSO.</li>
-            <li>Саветовања у вези вођења персоналних досијеа запослених.</li>
-            <li>Вођење евиденције рада запослених.</li>
-          </ul>
-        </div>
+        <Paragraph isMobile={false} />
       </article>
     </section>
   );
---
+++ src/components/Paragraph.tsx
@@ -0,0 +1,30 @@
+interface ParaProps {
+  isMobile: boolean;
+}
+
+const Paragraph = ({ isMobile }: ParaProps) => {
+  const mobileStyling =
+    'bg-gray-100 h-2/3 flex justify-center items-center text-start text-lg md:text-3xl pt-48 pb-24 md:px-16 px-8 ml-4 font-light';
+  const desktopStyling =
+    'bg-gray-100 h-2/3 flex justify-center items-center text-start text-2xl py-64 px-48';
+  return (
+    <div className={isMobile ? mobileStyling : desktopStyling}>
+      <ul className="list-disc">
+        <li>
+          Саветовање и израда правилника о организацији и систематизацији
+          послова.
+        </li>
+        <li>Саветовања и израда правилника о раду и општих аката.</li>
+        <li>
+          Израда уговора о раду/ анекса уговора о раду/ уговора о обављању
+          привремених и повремених послова/ уговора о делу.
+        </li>
+        <li>Израда свих решења из области радног односа.</li>
+        <li>Пријаве и одјаве запослених у CROSO.</li>
+        <li>Саветовања у вези вођења персоналних досијеа запослених.</li>
+        <li>Вођење евиденције рада запослених.</li>
+      </ul>
+    </div>
+  );
+};
+export default Paragraph;
