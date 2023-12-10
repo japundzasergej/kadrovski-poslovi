d97bab826fbdc8441a60c35593b7c9dd73041cc1
SERGEJ_JAPUNDZA <japundzasergej@gmail.com>
2023-12-10 16:02:31 +0100

    env

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -50,7 +50,7 @@ const Contact = () => {
           Контакт
         </h1>
         <form
-          action="https://formspree.io/f/xbjvndqd"
+          action={import.meta.env.VITE_FORM}
           method="POST"
           target="_blank"
           className="flex flex-col text-2xl min-w-[90%] lg:min-w-[50%]"
